a6e628970c473d61bd2896b7d7a95759d7c70d6f
Matt Calhoun <matt@gruntwork.io>
2017-09-18 23:01:57 -0500

    fix outputs

--- modules/consul-cluster/outputs.tf
+++ modules/consul-cluster/outputs.tf
@@ -11,5 +11,9 @@ output "load_balancer_ip_address" {
 }
 
 output "load_balancer_id" {
-  value = "${azurerm_public_ip.consul_access.*.id}"
+  value = "${azurerm_lb.consul_access.*.id}"
+}
+
+output "backend_pool_id" {
+  value = "${azurerm_lb_backend_address_pool.consul_bepool.*.id}"
 }
